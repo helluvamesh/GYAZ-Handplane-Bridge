6686a2b9b4633d3b5cc34fdaafc437bc875ff61a
helluvamesh <helluvamesh@gmail.com>
2018-07-03 23:47:09 +0200

    Add files via upload

---
+++ GYAZ_Handplane_Baker_Bridge__version_7/GYAZ_handplane_bridge/__init__.py
@@ -0,0 +1,64 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any laTter version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+
+##########################################################################################################
+##########################################################################################################
+
+bl_info = {   
+ "name": "GYAZ Handplane Baker Bridge",   
+ "author": "Andras Gyalog",   
+ "version": ('7'),   
+ "blender": (2, 7, 9),   
+ "location": "Properties>Rener>Handplane Bridge",   
+ "description": "Bridge between Blender and Handplane Baker.",
+ "warning": "",   
+ "wiki_url": "",   
+ "tracker_url": "",   
+ "category": "Import-Export"}
+
+modulesNames = ['handplane_bridge']
+ 
+import sys
+import importlib
+ 
+modulesFullNames = {}
+for currentModuleName in modulesNames:
+    modulesFullNames[currentModuleName] = ('{}.{}'.format(__name__, currentModuleName))
+ 
+for currentModuleFullName in modulesFullNames.values():
+    if currentModuleFullName in sys.modules:
+        importlib.reload(sys.modules[currentModuleFullName])
+    else:
+        globals()[currentModuleFullName] = importlib.import_module(currentModuleFullName)
+        setattr(globals()[currentModuleFullName], 'modulesNames', modulesFullNames)
+ 
+def register():
+    for currentModuleName in modulesFullNames.values():
+        if currentModuleName in sys.modules:
+            if hasattr(sys.modules[currentModuleName], 'register'):
+                sys.modules[currentModuleName].register()
+ 
+def unregister():
+    for currentModuleName in modulesFullNames.values():
+        if currentModuleName in sys.modules:
+            if hasattr(sys.modules[currentModuleName], 'unregister'):
+                sys.modules[currentModuleName].unregister()
+ 
+if __name__ == "__main__":
+    register()
