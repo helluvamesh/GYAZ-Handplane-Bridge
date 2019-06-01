589f0bcac6d4b100ef9667dd4bab83789487f731
helluvamesh <helluvamesh@gmail.com>
2019-06-01 06:37:02 +0200

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -23,7 +23,7 @@
 bl_info = {   
  "name": "GYAZ Handplane Baker Bridge",   
  "author": "Andras Gyalog",   
- "version": (2, 80, 4),   
+ "version": (2, 80, 5),   
  "blender": (2, 80, 0),   
  "location": "Properties>Rener>Handplane Bridge",   
  "description": "Bridge between Blender and Handplane Baker.",
